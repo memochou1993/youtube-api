8ad76bd5dfa0063201f8709c49ead27870cdaf6d
Memo Chou <memochou1993@gmail.com>
2018-10-29 01:51:41 +0800

    增加 select 與 where 等方法

--- src/Youtube.php
+++ src/Youtube.php
@@ -10,51 +10,86 @@ class Youtube
     protected $client;
     protected $url;
     protected $key;
+    protected $part;
     protected $params;
+    protected $q;
     
     public function __construct($key)
     {
         $this->client = new Client();
-
         $this->url = 'https://www.googleapis.com/youtube/v3';
-
         $this->key = $key;
+        $this->part = ['id', 'snippet'];
+        $this->params = [];
+        $this->q = '';
+    }
+
+    public function resource($type)
+    {
+        $this->url .= '/' . $type;
+
+        return $this;
+    }
+
+    public function select($part)
+    {
+        $this->part = is_array($part) ? $part : explode(', ', $part); 
+
+        return $this;
+    }
+
+    public function where()
+    {
+        if (func_num_args() == 1 && is_array(func_get_arg(0))) {
+            $this->params = func_get_arg(0);
+        }
+
+        if (func_num_args() == 2 && is_string(func_get_arg(0)) && is_string(func_get_arg(1))) {
+            $this->params[func_get_arg(0)] = func_get_arg(1);
+        }
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->request();
+    }
+
+    public function search($q)
+    {
+        $this->url .= '/search';
+
+        $this->q = $q;
+
+        return $this->request();
     }
 
     public function getChannel($username, $part = ['id', 'snippet', 'statistics'])
     {
         $this->url .= '/channels';
 
+        $this->part = is_array($part) ? $part : implode(', ', $part); 
+
         $this->params = [
             'forUsername' => $username,
-            'part' => implode(', ', $part),
         ];
 
-        return $this->getResponse($url);
+        return $this->request();
     }
 
-    public function getChannelVideos($channelId, $maxResults = 10, $part = ['id', 'snippet'])
+    protected function request()
     {
-        $this->url .= '/search';
-
-        $this->params = [
-            'type' => 'video',
-            'channelId' => $channelId,
-            'maxResults' => $maxResults,
-            'part' => implode(', ', $part),
-        ];
-
-        return $this->getResponse();
-    }
-
-    protected function getResponse()
-    {
-        $this->url .= '?key=' . $this->key . '&' . http_build_query($this->params);
+        $this->url .= 
+            '?' . 'key=' . $this->key . 
+            '&' . 'part=' . implode(', ', $this->part) . 
+            '&' . http_build_query($this->params);
 
         try {
             $response = $this->client->get($this->url)->getBody();
 
             echo json_encode([
+                'url' => $this->url,
                 'success' => true,
                 'response' => json_decode($response),
             ]);
@@ -62,8 +97,9 @@ class Youtube
             $response = $e->getResponse()->getBody()->getContents();
 
             echo json_encode([
+                'url' => $this->url,
                 'success' => false,
-                'response' => json_decode($response)->error,
+                'response' => json_decode($response),
             ]);
         }
     }
